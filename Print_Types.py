6828af6d3f1266956fb4b18472ab3a1bf8c1423c
ruthvik-9 <102401715+ruthvik-9@users.noreply.github.com>
2022-03-27 10:23:58 +0530

    Printing Types
    Different ways of printing a message and executing the code.

---
+++ Print_Types.py
@@ -0,0 +1,84 @@
+# print()
+# print("")
+# print('')
+# print(''' ''')
+# print(""" """)
+# print("""
+# """)
+# print('Hello python')
+# print("hello pytho")
+# print('hello wor"ld')
+# print("hello pyth'hon")
+# print('''hello
+# world''')
+# print('''hello
+#             world''')
+# print("""Hello
+
+#           World""")
+# print('''Hello
+
+#     World''')
+# print('''hello python''')
+# print("""hello python""")
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+# print('hello pyt"hon')
+# print("hello pyth'on")
+# print('''hello pyt'hon''')
+# print("""hello pyth'on""")
+# print('print(hello python)')
+# print("print(hello python)")
+# print('''print(heLLo Python)''')
+# print("""print(hello python)""")
+# print('print(hello wor"ld)')
+# print('''print(hello wor'ld)''')
+# print("""print(hello wor'ld)""")
+# print("print(hello world')")
+# print('''print(hello world')''')
+# print("""print(hello world")""")
+
+# print('hello\
+# world')
+
+# print('hello wor\'ld')
+# print('hello world\'')
+# print('hi hello\
+#  how are you?')
+
+# print('hello''world')
+# print('hello ''world')
+# print('hello' 'world')
+# print('hello'           'world')
+# print('hello'
+
+# 'world')
+
+# print('hello','python')
+# print('hello' , 'python')
+# print('hello'        ,            'world')
+# print('hello'   ,
+
+#        'world')
+
+# print(type('python'))
+# print(type(2))
+# print(type(22.5))
+# print(type(22/5))
+# print(type(20/5))
+# print(type(20*5))
+# print(type(20+5))
+# print(type(20+22.5))
+# print(type(20.5+22.5))
+
+# print('python'*5)
+# print('python '*8)
+# print(5*' hello')
+# print('123 ' * 10)
+
+
+# print('1 ' *1,
+# '2'*2,
+# '3'*3)
