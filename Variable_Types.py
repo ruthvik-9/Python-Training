b6286fc524bfa92e52d827fcc8e9093004a99bbb
ruthvik-9 <102401715+ruthvik-9@users.noreply.github.com>
2022-03-28 23:01:06 +0530

    Variable Types
    Different ways to assign variable and .isidentifier to know weather the variable passed is valid or not.

---
+++ Variable_Types.py
@@ -0,0 +1,135 @@
+
+# print('1 ' *1,
+# '2'*2,
+# '3'*3)
+
+
+# _=1234
+# _a=1234
+# print(_+_a)
+
+# # Swap Case
+
+# a=2
+# b=5
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+
+# a=5
+# b=2
+# a,b = b,a
+# print(a,b)
+
+
+
+# a=input('Enter a value : ')
+# b=input('Enter a value : ')
+# # c=a
+# # a=b
+# # b=c
+# a,b = b,a
+# print(a,b)
+
+# a=5
+# b=1
+# a,b=b,a
+# print(a,b)
+
+# a=5
+# A=2
+# _b=3
+# _1=4
+# c_a=5
+# v_3=6
+# _=7
+# x_u3=3
+# r6=9
+# t_4_d=10
+
+# print(a)
+# print(A)
+# print(_b)
+# print(_1)
+# print(c_a)
+# print(v_3)
+# print(_)
+# print(x_u3)
+# print(r6)
+# print(t_4_d)
+
+# a="python"
+# print(a.isidentifier())
+
+# b=" hello"  spaces not allowed inside vairable syntax Error
+# print(b.isidentifier())
+
+# c='23hf'
+# print(c.isidentifier())
+
+# d="p_y"
+# print(d.isidentifier())
+
+# e='_'
+# print(e.isidentifier())
+
+# f='_@45'
+# print(f.isidentifier())
+
+# g=('a v')
+# print(g.isidentifier())
+
+# a=1
+# b=2
+# c=3
+# d=2
+# e=4
+# print(a) 
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+
+# a=1
+# b=2
+# c=3
+# d=2
+# e=4
+
+# print(a);print(b);print(c);
+# print(d);print(e)
+
+
+# a=1
+# b=2
+# c=3
+# d=2
+# e=4
+# print(a), print(b), print(c), print(d), print(e)
+# print(a); print(b); print(c); print(d); print(e)
+# print(a,b,c,d,e)
+# print(e,e,b,e,a)
+
+
+# # a=1, b=2, c=3, d=2, e=4  
+# a=1; b=2; c=3; d=2; e=4  
+# print(a); print(b); print(c); print(d); print(e)
+# print(a,b,c,d,e) 
+# print(a,b,c,d,e,f) 
+
+
+
+# a,b,c,d,e=1,2,3,4,5
+# print(a,b,c,d,e) 
+# print(a);print(b);print(c);print(d);print(e)
+
+# a=d=2;b,c,e=1,3,4
+# print(a,b,c,e,d) 
+# print(a);print(b);print(c)
+
+# a=1;b=2
+
+# print(a+b)
