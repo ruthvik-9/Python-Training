a2057c558291b7f1f89efffd53701f94f68f46cf
ruthvik-9 <102401715+ruthvik-9@users.noreply.github.com>
2022-03-31 22:13:27 +0530

    Data Types and str concatenation
    different types of data types and concatenation types.

---
+++ data_types.py
@@ -0,0 +1,83 @@
+'''Str Concatenation'''
+
+# print('py'*3)
+# print('py '*3)
+# print('py'+'thon')
+# print('py '+'thon')
+# print('py'+''+'thon')
+# print('py'+' '+'thon')
+# print('py','thon')
+# print('python'+' '+'3.10.3')
+
+'''Data Types'''
+# a=4
+# print(type(a))
+# print(float(a))
+# print(str(a))
+
+# b=75.8
+# print(type(b))
+# print(int(b))
+# print(str(b))
+
+# a=3
+# print('the value of a is',a)
+# print('the value of a is ', a)
+
+# b=""
+# print(type(b))
+# print(b,type(b))
+
+# d=88
+# print(type(d))
+# print(int(d))
+# print(float(d))
+# print(str(d))
+
+# b=0.2
+# print(type(b))
+# print(int(b))    
+# print(float(b))
+# print(str(b))
+'''float conversion to int,considers only value before point'''
+
+# d="None"
+# print(type(d))
+# print('one of the data type is',d)
+# print(''+d)
+# print(' '+d)
+# print('one of the data type is'+d)
+# print('one of the data type is '+d)
+# print('one of the data type is'+''+d)
+# print('one of the data type is'+' '+d)
+
+# e='-20'
+# print(e,type(e))
+# print(int(e))
+# print(float(e))
+
+# print(int(5.6))
+# print(float(22.22))
+# print(int('pyh'))            
+# print(float('pyh'))
+'''int/float can't convert text'''
+
+# e='22.05'
+# print(str(e))
+# print(float(e))
+# print(int(e))               
+# m=float(e)
+# print(m)
+# print(int(m))
+'''float within str convert to int is Not possible,need to take extra vairable '''
+
+# h='25'
+# print(int(h))
+# print(float(h))
+# print(str(h))
+
+# h='3'
+# print('py'*int(h))
+# print(str(h)*int(h))
+# print(h*int(h))
+# print(float(h)*int(h))
