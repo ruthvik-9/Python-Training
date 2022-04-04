259fdd15288b3231cbfd2af9e07cec57ec2d7cb4
ruthvik-9 <102401715+ruthvik-9@users.noreply.github.com>
2022-04-04 22:25:31 +0530

    Type conversion and Format specifier

---
+++ typeconversion_formatspecifier.py
@@ -0,0 +1,92 @@
+# p=12
+# print(p,type(p))
+# print('the value of a is',p)
+# print('the value of a is %d'%p)
+# print('the value of a is %f'%p)
+# print('the value of a is %s'%p)
+
+# m=8.19
+# print(m,type(m))
+# print('the value of m is',m)
+# print('the value of m is %d'%m)
+# print('the value of m is %f'%m)
+# print('the value of m is %s'%m)
+
+# b=2.95
+# print('the value of c is %.1f'%b)
+# print('the value of c is %.2f'%b)
+# print('the value of c is %s'%b)
+
+# d=2
+# print('the value of d is %f'%d)
+
+# g=0.3+0.2
+# print('%f'%g)
+# print('%.1f'%g)
+# print('%2f'%g)
+# print('%.3f'%g)
+# print('%7f'%g)
+# print('%8f'%g)
+# print('%9f'%g)
+# print('%10f'%g)
+# print('%11f'%g)
+
+# b=95.342
+# print('%1f'%b)
+# print('%2f'%b)
+# print('%3f'%b)
+# print('%4f'%b)
+# print('%5f'%b)
+# print('%6f'%b)
+# print('%7f'%b)
+# print('%8f'%b)
+# print('%9f'%b)
+# print('%10f'%b)
+# print('%11f'%b)
+# print('%12f'%b)
+# print('%13f'%b)
+
+# x=0.5+0.9
+# y=0.8+0.6
+# z=0.3+0.2
+# print(x)
+# print(y)
+# print(z)
+# print('the value of x is %.2f'%x)
+# print('the value of x is %.1f'%y)
+# print('the value of x is %.1f'%z)
+
+# j='apple'
+# print('%d'%j)
+# print('%f'%j)
+# print('the fruit name is %s'%j)
+# print('the fruit name is%s'%j)
+# print('the fruit name is',j)
+
+# a='rut' ; b='h'; c='vik'
+# print(a+b+c)
+# print(a,b,c)
+# print(a+b+' '+c)
+# print(a+b+chr(32)+c)
+
+# a='8' ; b='10' ; c=2.8
+# print(a,b+str(c))
+# print(a+b+str(c))
+
+# a=65 ; b=32 ; c=98
+# print('%c'%a)
+# print('%c'%b)
+# print('%c'%c)
+
+# print(chr(88))
+# print(chr(95))
+# print(chr(100))
+
+# print(ord('A'))
+# print(ord('@'))
+# a='@'
+# print(ord(a))
+# print(ord('9'))
+# print(ord('Z'))
+# print(ord('#'))
+# print(chr(122))
